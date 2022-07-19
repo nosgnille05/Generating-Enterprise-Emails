cfd5fa5f2980908e587a846ca33ded0d0ede876e
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-07-19 21:55:38 +0000

    Lowercase Handling and Username Generation

--- .ccls-cache/@home@runner@Generating-Enterprise-Emails/main.c
+++ .ccls-cache/@home@runner@Generating-Enterprise-Emails/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define frand() (rand()/(double)RAND_MAX)
-#define nrand() (sqrt(-2*log(frand()))*cos(2*M_PI*frand()))
+#include <string.h>
+#include <ctype.h>
 
   //Gather Enterprises Domain Name
   //Read Employee Directory File
@@ -16,33 +16,46 @@
 int main(void) {
   srand(time(NULL));
   FILE *fp;
-  char buff[255];
-  char employee_first_name[255];
-  char employee_last_name[255];
-  char employee_first_last_name[255];
-  char enterprise_domain[50];
-  char enterprise_first_last_file[50];
+  char employee_first_name[100];
+  char employee_first_initial[100];
+  char employee_last_name[100];
+  char employee_first_last_name[200];
+  char enterprise_domain[100];
+  char enterprise_directory_file[100];
   char employee_random_char[10];
+  char employee_first_name_lowercase[50];
   int employee_random_num;
+  int one_email_generated = 0;
+  int random_nums_generated;
 
   //Get the Domain Name From User
   printf("What is your domain name? Ex. bing.com\n");
   scanf("%s", enterprise_domain);
+  printf("\n");
   //Get the First Last Name File Name From User
-  printf("What is the file path of your First-Last-Name-File?\n");
-  scanf("%s", enterprise_first_last_file);
-  
+  printf("What is the file path of your Directory-File?\n");
+  //scanf("%s", enterprise_directory_file);
+  printf("\n");
+
+  //Open File
   fp = fopen("Student Directory Sample.txt", "r");
   if (fp == NULL)
-    printf("ERROR! File not opened. (EXIT)");
-  int i=0;
-  while(fscanf(fp,"%s", employee_first_name) != EOF){
-    fscanf(fp, "%s", employee_last_name);
-    strcat(employee_first_name, employee_last_name);
-    strcat(employee_first_last_name, employee_first_name);
+    printf("ERROR! File not found. (EXIT)");
 
-    int j = 0;
-    while(j<3){
+  //Begin Email Generation
+  while(fscanf(fp,"%s", employee_first_name) != EOF){
+    employee_first_initial[0] = tolower(employee_first_name[0]);
+    fscanf(fp, "%s", employee_last_name);
+    employee_last_name[0] = tolower(employee_last_name[0]);
+    employee_first_name[0] = '\0';
+    strcat(employee_first_name, employee_first_initial);
+    strcat(employee_first_name, employee_last_name);
+    employee_last_name[0] = '\0';
+    employee_first_initial[0] = '\0';
+
+    //Generate 3 Random Numbers for Uniqueness
+    random_nums_generated = 0;
+    while(random_nums_generated<3){
       employee_random_num = rand()%10;
         if(employee_random_num == 0)
           strcat(employee_random_char, "0");
@@ -66,14 +79,16 @@ int main(void) {
           strcat(employee_random_char, "9");
         strcat(employee_first_last_name, employee_random_char);
         employee_random_char[0] = '\0';
-    j++;
+    random_nums_generated++;
     }
-    strcat(employee_first_last_name, "@");
-    strcat(employee_first_last_name, enterprise_domain);
-    printf("%s\n", employee_first_last_name);
-    employee_first_last_name[0] = '\0';
-  i++;
+
+    //Concate "User Name" with Enterprise Domain Name
+    strcat(employee_first_name, "@");
+    strcat(employee_first_name, enterprise_domain);
+    printf("%s\n", employee_first_name);
+  one_email_generated++;
   }
+  //Close File
   fclose(fp);
   
   return 0;
