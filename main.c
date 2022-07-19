c0eb2ca75be9ffc8dc093b17ad506a63d1cae165
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-07-19 20:53:11 +0000

    String Handling, User Input

--- main.c
+++ main.c
@@ -9,8 +9,7 @@
   //Read Employee Directory File
   //Tanner Ellingson -> telli
   //add @
-  //add enterprisename
-  //add .com
+  //add Enterprise Domain
   //Write to Employee Emails File
   //Close File(s)
 
@@ -21,39 +20,60 @@ int main(void) {
   char employee_first_name[255];
   char employee_last_name[255];
   char employee_first_last_name[255];
+  char enterprise_domain[50];
+  char enterprise_first_last_file[50];
+  char employee_random_char[10];
   int employee_random_num;
-  //char **employee_random_char[255];
+
+  //Get the Domain Name From User
+  printf("What is your domain name? Ex. bing.com\n");
+  scanf("%s", enterprise_domain);
+  //Get the First Last Name File Name From User
+  printf("What is the file path of your First-Last-Name-File?\n");
+  scanf("%s", enterprise_first_last_file);
   
   fp = fopen("Student Directory Sample.txt", "r");
   if (fp == NULL)
     printf("ERROR! File not opened. (EXIT)");
   int i=0;
   while(fscanf(fp,"%s", employee_first_name) != EOF){
-    //printf(" First : %s\n", employee_first_name);
     fscanf(fp, "%s", employee_last_name);
-    //printf("  Last : %s\n", employee_last_name);
-    strcat(employee_first_name,employee_last_name);
-    strcat(employee_first_last_name,employee_first_name);
-    printf("%s", employee_first_last_name);
+    strcat(employee_first_name, employee_last_name);
+    strcat(employee_first_last_name, employee_first_name);
+
+    int j = 0;
+    while(j<3){
+      employee_random_num = rand()%10;
+        if(employee_random_num == 0)
+          strcat(employee_random_char, "0");
+        if(employee_random_num == 1)
+          strcat(employee_random_char, "1");
+        if(employee_random_num == 2)
+          strcat(employee_random_char, "2");
+        if(employee_random_num == 3)
+          strcat(employee_random_char, "3");
+        if(employee_random_num == 4)
+          strcat(employee_random_char, "4");
+        if(employee_random_num == 5)
+          strcat(employee_random_char, "5");
+        if(employee_random_num == 6)
+          strcat(employee_random_char, "6");
+        if(employee_random_num == 7)
+          strcat(employee_random_char, "7");
+        if(employee_random_num == 8)
+          strcat(employee_random_char, "8");
+        if(employee_random_num == 9)
+          strcat(employee_random_char, "9");
+        strcat(employee_first_last_name, employee_random_char);
+        employee_random_char[0] = '\0';
+    j++;
+    }
+    strcat(employee_first_last_name, "@");
+    strcat(employee_first_last_name, enterprise_domain);
+    printf("%s\n", employee_first_last_name);
     employee_first_last_name[0] = '\0';
-    
-    employee_random_num = rand()%1000 + '0';
-    //strcat(**employee_random_char, employee_random_num);
-    printf("%d", employee_random_num);
-
-    printf("\n");
-      i++;
+  i++;
   }
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
   fclose(fp);
   
   return 0;
