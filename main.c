8436b320f378a9ad0fe4defe10f6fcb8cf8d4f87
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-07-19 01:45:25 +0000

    First, Last, Unique 3 Digits Added

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#define frand() (rand()/(double)RAND_MAX)
+#define nrand() (sqrt(-2*log(frand()))*cos(2*M_PI*frand()))
 
   //Gather Enterprises Domain Name
   //Read Employee Directory File
@@ -10,22 +15,35 @@
   //Close File(s)
 
 int main(void) {
+  srand(time(NULL));
   FILE *fp;
   char buff[255];
   char employee_first_name[255];
   char employee_last_name[255];
+  char employee_first_last_name[255];
+  int employee_random_num;
+  //char **employee_random_char[255];
   
   fp = fopen("Student Directory Sample.txt", "r");
   if (fp == NULL)
     printf("ERROR! File not opened. (EXIT)");
-  
-  fscanf(fp, "%s", employee_first_name);
-    printf(" First : %s\n", employee_first_name);
-   
-  fscanf(fp, "%s", employee_last_name);
-    printf("  Last : %s\n", employee_last_name);
+  int i=0;
+  while(fscanf(fp,"%s", employee_first_name) != EOF){
+    //printf(" First : %s\n", employee_first_name);
+    fscanf(fp, "%s", employee_last_name);
+    //printf("  Last : %s\n", employee_last_name);
+    strcat(employee_first_name,employee_last_name);
+    strcat(employee_first_last_name,employee_first_name);
+    printf("%s", employee_first_last_name);
+    employee_first_last_name[0] = '\0';
+    
+    employee_random_num = rand()%1000 + '0';
+    //strcat(**employee_random_char, employee_random_num);
+    printf("%d", employee_random_num);
 
-  printf("Concate: %s\n", strcat(employee_first_name,employee_last_name));
+    printf("\n");
+      i++;
+  }
 
   
   
