69c93162f9dd937c345a8b7f5b74665aeff1ae1d
Tanner Ellingson <98973755+nosgnille05@users.noreply.github.com>
2022-07-20 23:54:33 +0000

    Write output to file. Accept input file path from user. Clean comments.

--- main.c
+++ main.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
 #include <string.h>
 #include <ctype.h>
 
-  //Gather Enterprises Domain Name
-  //Read Employee Directory File
-  //Tanner Ellingson -> telli
-  //add @
-  //add Enterprise Domain
-  //check for duplicates
-  //Write to Employee Emails File
-  //Close File(s)
-
 int main(void) {
-  srand(time(NULL));
-  FILE *fp;
+  FILE *in;
   FILE *out;
+  char employee_first_initial[10];
   char employee_first_name[100];
-  char employee_first_initial[100];
   char employee_last_name[100];
-  char employee_last_4[100];
-  char employee_first_last_name[200];
+  char employee_last_name_first_four[10];
   char enterprise_domain[100];
   char enterprise_directory_file[100];
   char employee_random_char[10];
-  char employee_first_name_lowercase[50];
   int employee_random_num;
   int one_email_generated = 0;
   int random_nums_generated;
@@ -34,38 +20,40 @@ int main(void) {
   //Get the Domain Name From User
   printf("What is your domain name? Ex. bing.com\n");
   scanf("%s", enterprise_domain);
-  printf("\n");
+  
   //Get the First Last Name File Name From User
-  //printf("What is the file path of your Directory-File?\n");
-  //scanf("%s", enterprise_directory_file);
-  //printf("\n");
+  printf("\nWhat is the file path of your Directory-File?\n");
+  scanf("%s", enterprise_directory_file);
 
   //Open Input File
-  fp = fopen("Directory.txt", "r");
-  if (fp == NULL)
-    printf("ERROR! File not Opened. (EXIT)");
+  in = fopen(&enterprise_directory_file, "r");
+  if (in == NULL){
+    printf("We're unable to find the file/path you entered.\nPlease try again later.\n");
+    return 1;
+    }
   
   //Open Output File
   out = fopen("Result.txt","w");
-  if (out == NULL)
-    printf("ERROR! File not Opened. (EXIT)");
-  
+  if (out == NULL){
+    printf("Result file did not open correctly.\nPlease try again later.\n");
+    return 1;
+    }
 
   //Print Header
-  printf("Enterprise Emails:\n\n");
+  printf("\nEnterprise Emails:\n\n");
   //Begin Email Generation - Scan First Name from File
-  while(fscanf(fp,"%s", employee_first_name) != EOF){
+  while(fscanf(in,"%s", employee_first_name) != EOF){
     //Get the First Initial and Convert to Lowercase
     employee_first_initial[0] = tolower(employee_first_name[0]);
     //Scan Last Name - Convert to Lowercase - Only take first four characters
-    fscanf(fp, "%s", employee_last_name);
+    fscanf(in, "%s", employee_last_name);
     employee_last_name[0] = tolower(employee_last_name[0]);
-    memcpy(employee_last_4,&employee_last_name[0], 4);
+    memcpy(employee_last_name_first_four,&employee_last_name[0], 4);
     //Clear employee_first_name
     employee_first_name[0] = '\0';
     //Concate first initial and last name last four
     strcat(employee_first_name, employee_first_initial);
-    strcat(employee_first_name, employee_last_4);
+    strcat(employee_first_name, employee_last_name_first_four);
 
     //Generate 3 Random Numbers for Uniqueness
     random_nums_generated = 0;
@@ -93,20 +81,22 @@ int main(void) {
           strcat(employee_random_char, "9");
         strcat(employee_first_name, employee_random_char);
         employee_random_char[0] = '\0';
+    //Generate Next Random Char (3 Total)
     random_nums_generated++;
     }
 
     //Concate "User Name" with Enterprise Domain Name
     strcat(employee_first_name, "@");
     strcat(employee_first_name, enterprise_domain);
-    //EDIT ME (Check for Duplicates)
-    //EDIT ME (Write Output to File)
+    
+    //Write Output to File
     fprintf(out,"%s\n", employee_first_name);
     printf("%s\n", employee_first_name);
+  //Move to next Directory Entry
   one_email_generated++;
   }
-  //Close File
-  fclose(fp);
+  //Close Input/Output Files
+  fclose(in);
   fclose(out);
   
   return 0;
